927b5c9b039a75106dc87151cdbccb1559e5ae3d
krunal-more <rajeshrimore67@gmail.com>
2025-12-03 20:34:23 +0530

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,11 @@
+function goSorry() {
+    window.location.href = "decision.html";
+}
+
+function goSuccess() {
+    window.location.href = "pages/success.html";
+}
+
+function goNotOkay() {
+    window.location.href = "pages/sorry.html";
+}
